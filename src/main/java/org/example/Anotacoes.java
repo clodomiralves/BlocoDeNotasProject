b996b68cbda27ed0b480ee1107e800723665c84a
Clodomir Alves de Oliveira Junior <clodomir.alves@labs.dcx>
2023-04-26 10:56:32 -0300

    Enviando o primeiro commit

---
+++ src/main/java/org/example/Anotacoes.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Anotacoes {
+    //Atributos
+    private int id;
+    private String texto;
+    private boolean deletar;
+
+    public String getTexto(){
+        return texto;
+    }
+    public void setTexto(String texto){
+        this.texto = texto;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+}
---
+++ src/main/java/org/example/BlocoDeNotas.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class BlocoDeNotas {
+    ArrayList<Anotacoes> arrayAnotacoes = new ArrayList<>();
+    ArrayList<Anotacoes> arrayRemovidos = new ArrayList<>();
+
+    public void adicionaAnotacao(Anotacoes anotacao){
+        arrayAnotacoes.add(anotacao);
+    }
+    public 
+}
